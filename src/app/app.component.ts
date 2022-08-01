9a787bcda4f3530f1ef8b5e15209cbdbc5cf11ca
took025 <36102272+took025@users.noreply.github.com>
2022-08-01 21:59:37 +0400

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { mainService } from './core/services/main-service';
 
@@ -9,6 +9,13 @@ import { mainService } from './core/services/main-service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  clicked = false;
+  constructor(private elementRef: ElementRef) {
 
-
+  }
+  onClickMenu(e:any) {
+    const menu = document.getElementById('menu');
+    menu?.classList.toggle('active');  
+    this.clicked = !this.clicked
+  }
 }
